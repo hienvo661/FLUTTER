69334433b4d1e51a59f1b9141c64a36200996310
Võ Ngọc Hiện <63461448+hienvo661@users.noreply.github.com>
2022-07-11 21:24:48 +0700

    update UI
    Design UI restaurant Manage

--- food_app/lib/Widget/Restaurant_manage.dart
+++ food_app/lib/Widget/Restaurant_manage.dart
@@ -1,10 +1,14 @@
-// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use, avoid_print, dead_code
+
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:food_app/Widget/RatingStart.dart';
 import 'package:food_app/data/data.dart';
 import 'package:food_app/fontstyle/fontstyle.dart';
+import 'package:food_app/models/food.dart';
 import 'package:food_app/models/restaurant.dart';
+import 'package:like_button/like_button.dart';
 
 class RestaurantManage extends StatefulWidget {
   final Restaurant restaurant;
@@ -15,6 +19,85 @@ class RestaurantManage extends StatefulWidget {
 }
 
 class _RestaurantManageState extends State<RestaurantManage> {
+  _buildMenuItem(Food menuItem) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: 170,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(menuItem.imageUrl), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        Container(
+          height: 170,
+          width: 170,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.topLeft,
+                colors: [
+                  Colors.black12.withOpacity(0.1),
+                  Colors.black12.withOpacity(0.1),
+                  Colors.black12.withOpacity(0.2),
+                  Colors.black45.withOpacity(0.2),
+                ],
+                stops: [
+                  0.1,
+                  0.1,
+                  1,
+                  0.3,
+                ]),
+          ),
+        ),
+        Positioned(
+          bottom: 70,
+          child: Column(
+            children: [
+              Text(
+                '${menuItem.name}',
+                style: Appstyle.h1.copyWith(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '\$${menuItem.price.toString()}',
+                style: Appstyle.h1.copyWith(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 120,
+          left: 120,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.yellow[900],
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: IconButton(
+              onPressed: () {
+                print('Button Order');
+              },
+              icon: Icon(Icons.add),
+              iconSize: 30,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,14 +131,31 @@ class _RestaurantManageState extends State<RestaurantManage> {
                         size: 30,
                       ),
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.favorite,
-                        color: Colors.redAccent,
-                        size: 35,
+                    LikeButton(
+                      size: 32,
+                      circleColor: CircleColor(
+                          start: Color(0xff00ddff), end: Color(0xff0099cc)),
+                      bubblesColor: BubblesColor(
+                        dotPrimaryColor: Color(0xff0099cc),
+                        dotSecondaryColor: Color(0xff0099cc),
                       ),
+                      likeBuilder: (bool isLiked) {
+                        return Icon(
+                          Icons.favorite,
+                          color: isLiked ? Colors.red : Colors.white,
+                          size: 32,
+                        );
+                      },
                     ),
+
+                    // IconButton(
+                    //   onPressed: () {},
+                    //   icon: Icon(
+                    //     Icons.favorite,
+                    //     color: Colors.redAccent,
+                    //     size: 35,
+                    //   ),
+                    // ),
                   ],
                 ),
               ),
@@ -128,7 +228,20 @@ class _RestaurantManageState extends State<RestaurantManage> {
               'Menu',
               style: Appstyle.h1.copyWith(fontSize: 25, letterSpacing: 1),
             ),
-          )
+          ),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.all(10),
+              crossAxisCount: 2,
+              children: List.generate(
+                widget.restaurant.menu.length,
+                (index) {
+                  Food food = widget.restaurant.menu[index];
+                  return _buildMenuItem(food);
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
